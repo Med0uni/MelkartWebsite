c0ef724cc47e166956c376082994d192a3fb6eca
Mohamed Ouni <medouni13@gmail.com>
2020-12-27 00:32:28 +0100

    push2-add.0new.files

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function blog(props) {
+    
+    return (
+      <div>
+     
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    </div>
+    );
+  }
