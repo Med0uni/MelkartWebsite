91daf176b20138360dad1ae02830514755690db5
torjmen <torjmenspo@outlook.fr>
2021-02-18 23:01:15 +0100

    contact ready!

--- components/coordonnees.js
+++ components/coordonnees.js
@@ -1,24 +1,30 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import './coordonnees.css';
+import phone from "A:/MelkartWebsite/assets/img/ph.png";
+import email from "A:/MelkartWebsite/assets/img/envelope.png";
+import loc from "A:/MelkartWebsite/assets/img/local.png";
+
 
  export default function Coord() {
      return(
         <div>
-            <Grid container
-  direction="row"
-  justify="space-around"
-  alignItems="center">
-                <Grid item xs={4} spacing={1}>
-                    <p>phone here</p>
-                </Grid>
-                <Grid item xs={4} spacing={1}>
-                    <p>email here</p>
-                </Grid>
-                <Grid item xs={4} spacing={1}>
-                    <p>adress here</p>
-                </Grid>
-            </Grid>
+            <h2 className="ge">Get In Touch With Us!</h2>
+        <div className="cards">
+            <div className="card">
+                <img className="fe" src={phone}/>
+                <div>+216 102 163</div>
+
+            </div>
+            <div className="card">
+                <img className="fe" src={email}/>
+                <div>enicarthage.junior.entreprise@gmail.com</div>
+            </div>
+            <div  className="card">
+                <img className="fe" src={loc}/>
+                <div>45 Rue des Entrepreneurs 2035 Charguia II Tunis- Carthage-Tunisie</div>
+            </div> 
+        </div>
         </div>
      );
  }
