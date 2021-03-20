b75b138d0cac50c5e7ce8a6c3d3f3404ae885c3e
Med Hedi <55254329+Med13Hedi@users.noreply.github.com>
2021-03-20 22:54:40 +0100

    Add files via upload

--- pages/blog.js
+++ pages/blog.js
@@ -2,42 +2,73 @@ import React from "react";
 import fetch from 'isomorphic-unfetch';
 import Card from '../components/card';
 import Link from "next/link";
+import Button from "@material-ui/core/Button";
+import Router, { useRouter } from "next/router";
 
-
-export default function blog({posts}) {
+export default function blog({posts, page, nbreOfPosts}) {
   //console.log(posts)//
+  const router = useRouter();
+  // console.log(router.query);
+  const lastPage = Math.ceil(nbreOfPosts / 4);
   return (
-    <div className="container">
+    <div classNameName="container">
       <h1>Blogs</h1>
       <br/><br/>
 
-      <div className="row">
+      <div classNameName="row row-cols-2">
         {posts.map(post => (
           <Link href={`/${post.Slug}`} key={post.id}>
             
-            <div className="col-6">
-              <Card  post={post} />
+            <div classNameName="col">
+              <Card post={post} />
             </div>
             
           </Link>
+
         ))} 
+
+      <style jsx>{`
+        .col{
+          padding-top: 5px;
+          padding-left: 5px;
+          padding-right: 5px;
+          padding-bottom: 5px;
+        }
+      `}</style>
         
       </div>
 
+
+      <button className="btn btn-outline-dark"
+        onClick={()=> router.push(`/blog?page=${page-1}`) }
+        disabled={page <= 1} >
+          Previous</button>
+      <button className="btn btn-outline-dark"
+        onClick={()=> router.push(`/blog?page=${page+1}`) }
+        disabled={page >= lastPage} >
+          Next</button>
+      <br/><br/><br/><br/>
     </div>
   );
 }
 
-export async function getServerSideProps(){
+export async function getServerSideProps({query: {page =1} }){
   const { API_URL } = process.env;
 
-  const res= await fetch(`${API_URL}/posts`);
-  const data = await res.json();
+  const start = +page ===1 ? 0 : (+page -1 )* 4;
 
+  const nbreOfPostsResponse = await fetch(`${API_URL}/posts/count`);
+  const nbreOfPosts = await nbreOfPostsResponse.json();
+
+
+  const res= await fetch(`${API_URL}/posts?_limit=4&_start=${start}`);
+  const data = await res.json();
 
   return{
     props: {
-      posts: data
+      posts: data,
+      page: +page,
+      nbreOfPosts
     }
   }
 
