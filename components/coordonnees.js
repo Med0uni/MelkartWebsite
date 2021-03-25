a8308ad434945a838a86eeb1ddfc2652645d546c
Med Hedi <55254329+Med13Hedi@users.noreply.github.com>
2021-03-25 02:56:35 +0100

    Add files via upload

--- components/coordonnees.js
+++ components/coordonnees.js
@@ -1,54 +1,127 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import './coordonnees.css';
-import phone from "A:/MelkartWebsite/assets/img/ph.png";
-import email from "A:/MelkartWebsite/assets/img/envelope.png";
-import loc from "A:/MelkartWebsite/assets/img/local.png";
-import youtube from "A:/MelkartWebsite/assets/img/youtube.png";
-import facebook from "A:/MelkartWebsite/assets/img/facebook.png";
-import insta from "A:/MelkartWebsite/assets/img/instagram.svg";
-
 import phone from "../assets/img/ph.png";
 import email from "../assets/img/envelope.png";
 import loc from "../assets/img/local.png";
 
 
+ export default function Coord() {
+		const contactUS = async event => {
+			// event.preventDefault()
+			const { API_URL } = process.env;
 
+			const res = await fetch(`${API_URL}/messages`, {
+				body: JSON.stringify({
+					firstName: event.target.firstname.value,
+					lastName: event.target.lastname.value,
+					email: event.target.email.value,
+					subject: event.target.subject.value,
+					message: event.target.message.value,
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'POST'
+			})
 
+			const data = await res.json()
+		}
 
-export default function Coord() {
-    return (
+    return(
+			<div className="container-contact100">
+				<div className="wrap-contact100">
+					<form className="contact100-form validate-form" onSubmit={contactUS}>
+							<span className="contact100-form-title">
+								Send Us A Message
+							</span>
 
-        <div className=" col-xl-6 col-lg-6  col-md-12   col-sm-12" >
-            <div className="container bord">
-                <h2 className="ge">Restons en contact</h2>
-                <div >
+							<label className="label-input100" for="first-name">Tell us your name *</label>
+							<div className="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type first name">
+								<input id="firstname" className="input100" type="text" name="firstname" placeholder="First Name"/>
+								<span className="focus-input100"></span>
+							</div>
+							<div className="wrap-input100 rs2-wrap-input100 validate-input" data-validate="Type last name">
+								<input id="lastname" className="input100" type="text" name="lastname" placeholder="Last Name"/>
+								<span className="focus-input100"></span>
+							</div>
 
+							<label className="label-input100" for="email">Enter your email *</label>
+							<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+								<input id="email" className="input100" type="text" name="email" placeholder="Eg. example@email.com"/>
+								<span className="focus-input100"></span>
+							</div>
 
-                    <div> <img src={phone} /> <span className="he"> +216 102 163</span></div>
-                    <br></br>
+							<label className="label-input100" for="phone">Subject</label>
+							<div className="wrap-input100">
+								<input id="subject" className="input100" type="text" name="subject" placeholder="subject of your contact"/>
+								<span className="focus-input100"></span>
+							</div>
 
-                    <div><img src={email} /><span className="he">enicarthage.junior.entreprise@gmail.com</span></div>
-                    <br></br>
-                    <div><img src={loc} /><span className="he">45 Rue des Entrepreneurs 2035 Charguia II Tunis- Carthage-Tunisie</span></div>
+							<label className="label-input100" for="message">Message *</label>
+							<div className="wrap-input100 validate-input" data-validate = "Message is required">
+								<textarea id="message" className="input100" name="message" placeholder="Write us a message"></textarea>
+								<span className="focus-input100"></span>
+							</div>
 
-                </div>
-                <br />
-                <h2 className="ge">Suivez nous</h2>
-                <div >
+							<div className="container-contact100-form-btn">
+								<button className="contact100-form-btn">
+									Send Message
+								</button>
+							</div>
+					</form>
 
-                    <a href="https://www.facebook.com/Enicarthage.Junior.Entreprise"  target="popup">
-                        <img className="fe" src={facebook} />
-                    </a>
+					<div className="contact100-more ">
+							<div className="size1">
+								<div className="txt1 p-r-25">
+									<span className="lnr lnr-map-marker"></span>
+								</div>
 
-                    <a href="https://www.youtube.com/channel/UCB3dah_szpBGypXHMwuTveg"  target="popup">
-                        <img className="fe" src={youtube} />
-                    </a>
-                    <a href="https://www.instagram.com/melkart_je/"  target="popup">
-                        <img className="fe" src={insta} />
-                    </a>
+								<div className="size2">
+									<span className="txt1 p-b-20">
+										Address
+									</span>
 
-                </div>
-            </div>
-        </div>
+									<span className="txt2">
+										Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US
+									</span>
+								</div>
+							</div>
+
+							<div className="size1">
+								<div className="txt1 p-r-25">
+									<span className="lnr lnr-phone-handset"></span>
+								</div>
+
+								<div className="size2">
+									<span className="txt1 p-b-20">
+										Lets Talk
+									</span>
+
+									<span className="txt3">
+										+1 800 1236879
+									</span>
+								</div>
+							</div>
+
+							<div className="size1">
+								<div className="txt1 p-r-25">
+									<span className="lnr lnr-envelope"></span>
+								</div>
+
+								<div className="size2">
+									<span className="txt1 p-b-20">
+										General Support
+									</span>
+
+									<span className="txt3">
+										contact@example.com
+									</span>
+								</div>
+							</div>
+					</div>
+				</div>
+			</div>
+   
     );
-}
+ }
--- components/work-with-us.js
+++ components/work-with-us.js
@@ -1,56 +1,79 @@
 import React from "react";
 import './workwithus.css';
-import "../assets/img/examples/studio-3.jpg";
-import img1 from "../assets/img/project.jpg";
+
 
 export default function Workwithus() {
-    
-    return (
-		<div className="row col-12">
-		<div className="col-xl-6  col-lg-6  col-md-12 col-sm-12	parallax  " >
-		</div>
-		<div className="choose choose-form animate-box  col-xl-6 col-lg-6  col-md-12   col-sm-12 formulaire container ">
-		<div className="ftco-heading">
-			<h1 className='blanc title'>Start a project with us :</h1>
-		</div>
-		<form action="#">
-			<div className="row form-group ">
-				<div className="col-md-6">
-					<label className='blanc' for="fname">First Name</label>
-					<input className='inputa' type="text" id="fname" className="form-control"/>
-				</div>
-				<div className="col-md-6">
-					<label className='blanc' for="lname">Last Name</label>
-					<input className='input' type="text" id="lname" className="form-control"/>
-				</div>
-			</div>
+	const contactUS = async event => {
+    // event.preventDefault()
+		const { API_URL } = process.env;
 
-			<div className="row form-group">
-				<div className="col-md-12">
-					<label className='blanc' for="email">Email</label>
-					<input className='input' type="text" id="email" className="form-control"/>
-				</div>
-			</div>
+    const res = await fetch(`${API_URL}/messages`, {
+      body: JSON.stringify({
+        firstName: event.target.fname.value,
+				lastName: event.target.lname.value,
+				email: event.target.email.value,
+				subject: event.target.subject.value,
+				message: event.target.message.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    })
 
-			<div className="row form-group">
-				<div className="col-md-12">
-					<label className='blanc' for="subject">Subject</label>
-					<input className='input' type="text" id="subject" className="form-control"/>
-				</div>
-			</div>
-
-			<div className="row form-group">
-				<div className="col-md-12">
-					<label className='blanc' for="message">Message</label>
-					<textarea name="message" id="message" cols="30" rows="10" className="form-control"></textarea>
-				</div>
-			</div>
-			<div className="form-group">
-				<input type="submit" value="Send Message" className="btn btn-primary"/>
-			</div>
-
-		</form>	
-	</div>
-	</div>
-    );
+    const data = await res.json()
   }
+	    
+	return (
+	<div className="container-fluid">
+		<div className="row">
+			<div className="col-6 parallax "></div>
+			<div className="choose choose-form animate-box  col-6 formulaire container">
+				<div className="ftco-heading">
+					<h2 className='blanc'>start a project with us</h2>
+				</div>
+				<form onSubmit={contactUS}>
+					<div className="row form-group">
+						<div className="col-md-6">
+							<label className='blanc' for="fname">First Name</label>
+							<input type="text" id="fname" className="form-control" required/>
+						</div>
+
+						<div className="col-md-6">
+							<label className='blanc' for="lname">Last Name</label>
+							<input type="text" id="lname" className="form-control" required/>
+						</div>
+					</div>
+
+					<div className="row form-group">
+						<div className="col-md-12">
+							<label className='blanc' for="email">Email</label>
+							<input type="text" id="email" className="form-control" required/>
+						</div>
+					</div>
+
+					<div className="row form-group">
+						<div className="col-md-12">
+							<label className='blanc' for="subject">Subject</label>
+							<input type="text" id="subject" className="form-control" required/>
+						</div>
+					</div>
+
+					<div className="row form-group">
+						<div className="col-md-12">
+							<label className='blanc' for="message">Message</label>
+							<textarea name="message" id="message" cols="30" rows="10" className="form-control" required></textarea>
+						</div>
+					</div>
+					
+					<div className="form-group">
+						<input type="submit" value="Send Message" className="btn btn-primary"/>
+					</div>
+
+				</form>	
+			</div>
+		</div>
+	</div>
+	);
+}
+
