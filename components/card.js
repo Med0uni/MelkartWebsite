993c61a2163513de48805df8d7e5769638f1c184
Med Hedi <55254329+Med13Hedi@users.noreply.github.com>
2021-03-25 11:27:19 +0100

    Add files via upload

--- components/card.js
+++ components/card.js
@@ -13,7 +13,7 @@ export default function Card({post}) {
 
     <div className="card justify-content-center">
       <img src={API_URL + post.image[0].url} className="card-img-top" alt={post.title} />
-      <div className="card-body h-100 ">
+      <div className="card-body h-100">
         <h3 className="card-title font-weight-bolder text-dark">{post.title}</h3>
         <p className="card-text">{post.description}</p>
         <a href="#" className="card-link text-muted"><u>Read all</u></a>
@@ -21,7 +21,11 @@ export default function Card({post}) {
 
 
       <style jsx>{`
+        .card-title{
+          color: black;
+        }
         img{
+          height: 400px;
           padding-top: 5px;
           padding-left: 5px;
           padding-right: 5px;
