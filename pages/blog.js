4d806957b9c08d840f3820999730ed41ba90d365
Med Hedi <55254329+Med13Hedi@users.noreply.github.com>
2021-03-25 02:57:05 +0100

    Add files via upload

--- pages/blog.js
+++ pages/blog.js
@@ -2,24 +2,27 @@ import React from "react";
 import fetch from 'isomorphic-unfetch';
 import Card from '../components/card';
 import Link from "next/link";
-import Button from "@material-ui/core/Button";
 import Router, { useRouter } from "next/router";
 
 export default function blog({posts, page, nbreOfPosts}) {
-  //console.log(posts)//
+  //console.log(posts)
   const router = useRouter();
   // console.log(router.query);
   const lastPage = Math.ceil(nbreOfPosts / 4);
+
+  const elts = [...Array(lastPage)];
+  console.log("length: ",elts.length)
+
   return (
-    <div classNameName="container">
+    <div className="container-xl">
       <h1>Blogs</h1>
       <br/><br/>
 
-      <div classNameName="row row-cols-2">
+      <div className="row row-cols-2">
         {posts.map(post => (
           <Link href={`/${post.Slug}`} key={post.id}>
             
-            <div classNameName="col">
+            <div className="col">
               <Card post={post} />
             </div>
             
@@ -27,27 +30,75 @@ export default function blog({posts, page, nbreOfPosts}) {
 
         ))} 
 
-      <style jsx>{`
-        .col{
-          padding-top: 5px;
-          padding-left: 5px;
-          padding-right: 5px;
-          padding-bottom: 5px;
-        }
-      `}</style>
-        
       </div>
 
 
-      <button className="btn btn-outline-dark"
+      <div className="pagination float-right">
+        <button className="btn" onClick={()=> router.push(`/blog?page=${page-1}`) }
+        disabled ={page <= 1}>&laquo;
+        </button>
+
+        {elts.map((elt, index, array) => (
+          {...index === (page-1) ?   
+            <button className="btn active" onClick={()=> router.push(`/blog?page=${index+1}`)}>
+            {/* {console.log(index+1)} */}
+            {index+1}</button> : 
+            <button className="btn" onClick={()=> router.push(`/blog?page=${index+1}`)}>
+            {/* {console.log(index+1)} */}
+            {index+1}</button> 
+          }   
+        ))}
+
+        
+        <button className="btn" onClick={()=> router.push(`/blog?page=${page+1}`) }
+        disabled={page >= lastPage}>&raquo;
+        </button>   
+      </div>
+
+      {/* <button className="btn btn-outline-dark"
         onClick={()=> router.push(`/blog?page=${page-1}`) }
         disabled={page <= 1} >
           Previous</button>
       <button className="btn btn-outline-dark"
         onClick={()=> router.push(`/blog?page=${page+1}`) }
         disabled={page >= lastPage} >
-          Next</button>
+          Next</button> */}
       <br/><br/><br/><br/>
+      
+
+        <style global jsx>{`
+        .btn{
+          color: black;
+        }
+        .col{
+          padding-top: 5px;
+          padding-left: 2.5px;
+          padding-right: 2.5px;
+          padding-bottom: 5px;
+        }
+        .pagination {
+          display: inline-block;
+        }
+        
+        .pagination button {
+          color: black;
+          float: left;
+          padding: 8px 16px;
+          text-decoration: none;
+        }
+        
+        .pagination button.active {
+          background-color: #2f5b96;
+          color: white;
+          border-radius: 5px;
+        }
+        
+        .pagination button:hover:not(.active) {
+          background-color: #2f5b96;
+          border-radius: 5px;
+        }
+      `}</style>
+          {/* conatiner-xl */}
     </div>
   );
 }
@@ -55,14 +106,15 @@ export default function blog({posts, page, nbreOfPosts}) {
 export async function getServerSideProps({query: {page =1} }){
   const { API_URL } = process.env;
 
-  const start = +page ===1 ? 0 : (+page -1 )* 4;
+  const start = +page === 1 ? 0 : (+page - 1 )* 4;
+  // const start = 0;
 
   const nbreOfPostsResponse = await fetch(`${API_URL}/posts/count`);
   const nbreOfPosts = await nbreOfPostsResponse.json();
 
-
   const res= await fetch(`${API_URL}/posts?_limit=4&_start=${start}`);
   const data = await res.json();
+
 
   return{
     props: {
@@ -71,7 +123,4 @@ export async function getServerSideProps({query: {page =1} }){
       nbreOfPosts
     }
   }
-
-
-
 }
